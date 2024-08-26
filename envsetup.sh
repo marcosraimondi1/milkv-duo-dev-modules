d64780903a3965fb7960b2416a1a56c658397a76
marcosraimondi1 <marcosraimondi1@hotmail.com>
2024-08-26 19:06:20 -0300

    updated envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
-SDK_DIR=/home/marcos/facu/tesis/SDKs/duo-buildroot-sdk
+SDK_DIR?=/home/marcos/facu/tesis/SDKs/duo-buildroot-sdk
 cd $SDK_DIR
 source $SDK_DIR/device/milkv-duo256m-sd/boardconfig.sh > /dev/null
 source $SDK_DIR/build/milkvsetup.sh > /dev/null
@@ -9,4 +9,4 @@ defconfig cv1812cp_milkv_duo256m_sd > /dev/null
 cd $CURRENT_DIR
 # export ARCH=riscv
 # export CROSS_COMPILE=riscv64-unknown-linux-musl-
-export LINUXDIR=$SDK_DIR/linux_5.10/build/cv1812cp_milkv_duo256m_sd
+export LINUXDIR=$SDK_DIR/linux_5.10/build/kernel_output
